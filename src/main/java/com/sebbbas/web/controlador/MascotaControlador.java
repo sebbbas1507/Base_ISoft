af9e2fdc20d763632321120893b06600e72c8d0e
sebbbas1507 <jmonroys2@ucentral.edu.co>
2022-11-24 00:26:50 -0500

    Crud completo

--- src/main/java/com/sebbbas/web/controlador/MascotaControlador.java
+++ src/main/java/com/sebbbas/web/controlador/MascotaControlador.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.sebbbas.web.entidades.Mascota;
@@ -31,6 +32,34 @@ public class MascotaControlador {
 	public String guardarMascota(@ModelAttribute("mascota") Mascota mascota) {
 		servicio.guardarMascota(mascota);
 		return "redirect:mascotas";
+	}
+	@GetMapping("/mascotas/editar/{id}")
+	public String mostrarFormularioDeEditar(@PathVariable Long id, Model modelo) {
+		modelo.addAttribute("mascota", servicio.obtenerMascotaPorId(id));
+		return "editar_mascota";
+	}
+	@PostMapping("/maascotas/{id}")
+	public String actualizarMascota(@PathVariable Long id,@ModelAttribute("mascota") Mascota mascota, Model modelo) {
+		Mascota mascotaExistente = servicio.obtenerMascotaPorId(id);
+		mascotaExistente.setId(id);
+		mascotaExistente.setNombre(mascota.getNombre());
+		mascotaExistente.setIdentificacion(mascota.getIdentificacion());
+		mascotaExistente.setContrasena(mascota.getContrasena());
+		mascotaExistente.setTipo_animal(mascota.getTipo_animal());
+		mascotaExistente.setRaza(mascota.getRaza());
+		mascotaExistente.setSexo(mascota.getSexo());
+		mascotaExistente.setEdad(mascota.getEdad());
+		mascotaExistente.setNombre_dueno(mascota.getNombre_dueno());
+		mascotaExistente.setCelular(mascota.getCelular());
+		mascotaExistente.setDireccion(mascota.getDireccion());
+		servicio.actualizarMascota(mascotaExistente);
+		return "redirect:/mascota";
+	}
+	
+	@GetMapping("/mascotas/{id}")
+	public String eliminarMascota(@PathVariable Long id) {
+		servicio.eliminarMascota(id);
+		return "redirect:/mascotas";
 	}
 
 }
